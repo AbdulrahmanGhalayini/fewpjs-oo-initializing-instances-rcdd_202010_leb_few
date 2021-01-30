9adaab70b9f8e8da16713cc7ec4fdddd2eea04cf
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-01-30 00:46:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,23 @@ class Breakfast {
 let a = new Breakfast("eggs", "juice")
 
 
-// class breakfast {}
+class Lunch {
+  
+     constructor(salad, soup) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
 
 // let a =new breakfast()
+
+
+    newlunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
+    expect(newlunch.salad).to.equal('side salad')
+    expect(newlunch.soup).to.equal('broccoli cheddar soup')
+    expect(newlunch.drink).to.equal('iced tea')
+  })
 
 
 // class breakfast {}
